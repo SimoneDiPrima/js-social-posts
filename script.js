d067585b7e231a179ce5bbf881bdee0c183e8844
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-06 18:02:00 +0200

    aggiunta e stampa immagini

--- script.js
+++ script.js
@@ -17,52 +17,75 @@ const postArray = [
 {
     id : 1,
     nome: `Phil Mangione`,
-    userPic : `https://unsplash.it/300/300?image=15`,
+    userPic : `img/05.jpg`,
     birthDate : `04-02-1988`,
     text:`lorem ipsum not laborem success ecc`,
-    src:`https://unsplash.it/600/300?image=171`,
+    image:`img/01.jpg`,
     likes:85,
 
 },
 {
     id : 2,
     nome: `Enrico Campani`,
-    userPic : `https://unsplash.it/300/300?image=16`,
+    userPic : `img/04.jpg`,
     birthDate : `02-03-1990`,
     text:`lorem ipsum ecc`,
-    src:`https://unsplash.it/600/300?image=172`,
+    image:`img/02.jpg`,
     likes:75,
 
 },
 {
     id : 3,
     nome: `Fabrizio Mastrobattista`,
-    userPic : `https://unsplash.it/300/300?image=17`,
+    userPic : `img/03.jpg`,
     birthDate : `02-05-1994`,
     text:`lorem ipsum not laborem success ecc`,
-    src:`https://unsplash.it/600/300?image=173`,
+    image:`img/03.jpg`,
     likes:85,
 
 },
 {
     id : 1,
     nome: `Simone Di Prima`,
-    userPic : `https://unsplash.it/300/300?image=18`,
+    userPic : `img/02.jpg`,
     birthDate : `17-03-1992`,
     text:`lorem ipsum not laborem success ecc`,
-    src:`https://unsplash.it/600/300?image=174`,
+    image:`img/04.jpg`,
     likes:85,
 
 },
 {
     id : 1,
     nome: `Matilde Paparatto`,
-    userPic : `https://unsplash.it/300/300?image=19`,
+    userPic : `img/01.jpg`,
     birthDate : `07-02-1995`,
     text:`lorem ipsum not laborem success ecc`,
-    src:`https://unsplash.it/600/300?image=175`,
+    image:`img/05.jpg`,
     likes:85,
 
 }
 ]
-console.log(postArray);
+// console.log(postArray);
+
+// ora prendo gli elementi del dom dove devo stampare i miei post
+
+const container = document.getElementById(`container`);
+
+let posts = ` `;
+
+for(let i=0;i<postArray.length;i++){
+    const currentPost = postArray[i];
+   
+    for(let key in currentPost){
+        posts += `<div>${currentPost.nome}`
+        if(key === `userPic`){
+            posts += `<img src="${currentPost[key]}" alt="foto user"/>`;
+        }
+        else if(key === `image`){
+            posts += `<img src="${currentPost[key]}" alt="foto paesaggio"/>`; 
+        }
+    }
+    posts += `</div>`;
+    
+}
+container.innerHTML = posts;
